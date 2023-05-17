01ec1c48ebf8dc7f51811d4b6c653ed5e37f5dd5
bibitdharmawan <115470438+bibitdharmawan@users.noreply.github.com>
2023-05-17 12:32:21 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Layout Blog</title>
+	<link rel="stylesheet" type="text/css" href="css/w3.css">
+</head>
+<body>
+	<div class="w3-row w3-gray w3-margin w3-round">
+		<div class="w3-display-container" style="height: 50px;">
+			<div class="w3-display-middle">HEADER</div>
+		</div>
+	</div>
+	<div class="w3-row w3-gray w3-margin w3-round w3-center">NAVIGATION</div>
+	<div class="w3-row-padding">
+		<div class="w3-col l4">
+			<div class="w3-display-container w3-green w3-round" style="height: 200px;">
+				<div class="w3-display-middle">SIDEBAR</div>
+			</div>
+		</div>
+		<div class="w3-col l8">
+			<div class="w3-display-container w3-blue w3-round" style="height: 200px;">
+				<div class="w3-display-middle">CONTENT</div>
+			</div>
+		</div>
+	</div>
+	<div class="w3-row w3-gray w3-margin w3-round">
+		<div class="w3-display-container" style="height: 75px;">
+			<div class="w3-display-middle">FOOTER</div>
+		</div>
+	</div>
+</body>
+</html>
